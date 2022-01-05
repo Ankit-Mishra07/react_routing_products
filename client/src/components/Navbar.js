03e128eb5eac3d7d8542b709355ae619b1511236
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-05 18:14:52 +0530

    creating navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+
+import styled from "styled-components"
+
+const Nav = styled.nav`
+display: flex;
+justify-content: space-between;
+padding: 5px 10px;
+position: fixed;
+width: 100%;
+top: 0;
+`
+const Logo = styled.img`
+width: 100px;
+height : 60px;
+
+`
 
 const Navbar = () => {
     return (
-        <div>
+        <Nav>
+
+            <div>
+                <Logo src="https://th.bing.com/th/id/OIP.WCX6T5B4HlDDFUDfbRkpqgHaHa?w=169&h=180&c=7&r=0&o=5&pid=1.7" alt="" />
+                <SearchBox>
+                <i class="fal fa-search"></i>
+                <input type="search"  />
+                </SearchBox>
+            </div>
             
-        </div>
+        </Nav>
     )
 }
 
